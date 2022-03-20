ad7ea1a677b3b85ba6764892005d1634f4560619
NIPUN YADAV <89506943+Nyadav123@users.noreply.github.com>
2022-03-20 04:55:35 -0700

    Update MyNotes.js

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,23 +1,20 @@
 import React, { useEffect } from "react";
 import { Accordion, Badge, Button, Card } from "react-bootstrap";
 import MainScreen from "../../components/MainScreen";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 
 import { useDispatch, useSelector } from "react-redux";
-import { deleteNoteAction, listNotes } from "../../actions/notesActions";
-import Loading from "../../components/Loading";
-import ErrorMessage from "../../components/ErrorMessage";
+import { deleteNoteAction, listNotes } from "../../actions/notesAction";
+import Loading from "../../components/Header/Loading";
+import Error from "../../components/Header/Error";
 
-function MyNotes({ history, search }) {
+function MyNotes({ search }) {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const noteList = useSelector((state) => state.noteList);
   const { loading, error, notes } = noteList;
-
-  // const filteredNotes = notes.filter((note) =>
-  //   note.title.toLowerCase().includes(search.toLowerCase())
-  // );
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -57,80 +54,69 @@ function MyNotes({ history, search }) {
 
   return (
     <MainScreen title={`Welcome Back ${userInfo && userInfo.name}..`}>
-      {console.log(notes)}
       <Link to="/createnote">
         <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
           Create new Note
         </Button>
       </Link>
-      {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-      {errorDelete && (
-        <ErrorMessage variant="danger">{errorDelete}</ErrorMessage>
-      )}
+      {error && <Error variant="danger">{error}</Error>}
+      {errorDelete && <Error variant="danger">{errorDelete}</Error>}
       {loading && <Loading />}
       {loadingDelete && <Loading />}
-      {notes &&
-        notes
-          .filter((filteredNote) =>
-            filteredNote.title.toLowerCase().includes(search.toLowerCase())
-          )
-          .reverse()
-          .map((note) => (
-            <Accordion>
-              <Card style={{ margin: 10 }} key={note._id}>
-                <Card.Header style={{ display: "flex" }}>
-                  <span
-                    // onClick={() => ModelShow(note)}
-                    style={{
-                      color: "black",
-                      textDecoration: "none",
-                      flex: 1,
-                      cursor: "pointer",
-                      alignSelf: "center",
-                      fontSize: 18,
-                    }}
-                  >
-                    <Accordion.Toggle
-                      as={Card.Text}
-                      variant="link"
-                      eventKey="0"
-                    >
-                      {note.title}
-                    </Accordion.Toggle>
-                  </span>
+      {notes
+        ?.reverse()
+        .filter((filteredNote) =>
+          filteredNote.title.toLowerCase().includes(search.toLowerCase())
+        )
+        .map((note) => (
+          <Accordion>
+            <Card style={{ margin: 10 }} key={note._id}>
+              <Card.Header style={{ display: "flex" }}>
+                <span
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    cursor: "pointer",
+                    alignSelf: "center",
+                    fontSize: 18,
+                  }}
+                >
+                  <Accordion.Toggle as={Card.Text} variant="link" eventKey="0">
+                    {note.title}
+                  </Accordion.Toggle>
+                </span>
 
-                  <div>
-                    <Button href={`/note/${note._id}`}>Edit</Button>
-                    <Button
-                      variant="danger"
-                      className="mx-2"
-                      onClick={() => deleteHandler(note._id)}
-                    >
-                      Delete
-                    </Button>
-                  </div>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    <h4>
-                      <Badge variant="success">
-                        Category - {note.category}
-                      </Badge>
-                    </h4>
-                    <blockquote className="blockquote mb-0">
-                      <ReactMarkdown>{note.content}</ReactMarkdown>
-                      <footer className="blockquote-footer">
-                        Created on{" "}
-                        <cite title="Source Title">
-                          {note.createdAt.substring(0, 10)}
-                        </cite>
-                      </footer>
-                    </blockquote>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-          ))}
+                <div>
+                  <Button href={`/note/${note._id}`}>Edit</Button>
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <h4>
+                    <Badge variant="success">Category - {note.category}</Badge>
+                  </h4>
+                  <blockquote className="blockquote mb-0">
+                    <ReactMarkdown>{note.content}</ReactMarkdown>
+                    <footer className="blockquote-footer">
+                      Created on{" "}
+                      <cite title="Source Title">
+                        {note.createdAt.substring(0, 10)}
+                      </cite>
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        ))}
     </MainScreen>
   );
 }

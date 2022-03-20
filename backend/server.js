baf3f7df4dd0980278deba75d60c3981aa0dbbc0
NIPUN YADAV <89506943+Nyadav123@users.noreply.github.com>
2022-03-20 04:13:06 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,37 @@
 const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
+
 const app = express();
 dotenv.config();
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
+connectDB();
+app.use(express.json());
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-});
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
-// import express from "express";
-// import dotenv from "dotenv";
-// import connectDB from "./config/db.js";
-// import colors from "colors";
-// import path from "path";
-
-// import noteRoutes from "./routes/noteRoutes.js";
-// import userRoutes from "./routes/userRoutes.js";
-// import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
-
-// dotenv.config();
-
-// connectDB();
-
-// const app = express(); // main thing
-
-// app.use(express.json()); // to accept json data
-
-// app.use("/api/notes", noteRoutes);
-// app.use("/api/users", userRoutes);
-
-// // --------------------------deployment------------------------------
+app.use("/api/notes", noteRoutes);
+app.use("/api/users", userRoutes);
 // const __dirname = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-// // --------------------------deployment------------------------------
-
-// // Error Handling middlewares
-// app.use(notFound);
-// app.use(errorHandler);
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(
-//   PORT,
-//   console.log(
-//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
-//       .bold
-//   )
-// );
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is Running.....");
+  });
+}
+app.use(notFound);
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`
+  )
+);
